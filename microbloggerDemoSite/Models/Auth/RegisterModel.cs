3f9ed3a86c506989bdf6f6a6e638074d910d2418
Arnoldas Rudzevicius <arnoldas.rudz@gmail.com>
2016-06-03 06:13:55 +0100

    Fix form labels displayed on the registration page.

--- microbloggerDemoSite/Models/Auth/RegisterModel.cs
+++ microbloggerDemoSite/Models/Auth/RegisterModel.cs
@@ -11,20 +11,24 @@ namespace microbloggerDemoSite.Models.Auth
     public class RegisterModel
     {
         [Required]
+        [Display(Name = "Username (email)", Description = "Used to log in.")]
         [DataType(DataType.EmailAddress, ErrorMessage = "Username must be a valid email address.")]
         public string UserName { get; set; }
 
         [Required]
+        [Display(Name = "Nick", Description = "Your public name.")]
         [DataType(DataType.Text)]
         [StringLength(50, MinimumLength = 1, ErrorMessage = "Length must be between 1 and 50 characters.")]
         public string Nick { get; set; }
 
         [Required]
+        [Display(Name = "Password")]
         [DataType(DataType.Password)]
         [StringLength(100, MinimumLength = 8, ErrorMessage = "Length must be between 8 and 100 characters.")]
         public string Password { get; set; }
 
         [Required]
+        [Display(Name = "Password (again)")]
         [DataType(DataType.Password)]
         [StringLength(100, MinimumLength = 8, ErrorMessage = "Length must be between 8 and 100 characters.")]
         public string PasswordRepeat { get; set; }
